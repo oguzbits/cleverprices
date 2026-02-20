e1f1be321031211cd20590ecafffb065d86205c9
oguz <oguz.oeztuerk@outlook.de>
2026-02-21 00:44:30 +0100

    refactor: remove unnecessary Suspense wrapper from NuqsProvider.

--- src/providers/nuqs-provider.tsx
+++ src/providers/nuqs-provider.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import { Suspense } from "react";
 
 export function NuqsProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense>
-      <NuqsAdapter>{children}</NuqsAdapter>
-    </Suspense>
-  );
+  return <NuqsAdapter>{children}</NuqsAdapter>;
 }
