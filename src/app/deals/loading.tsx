1528607dcb8f7eb9865250b76763bd3dd1a2c943
oguz <oguz.oeztuerk@outlook.de>
2026-02-22 16:06:07 +0100

    refactor: remove loading skeletons for deals and category pages

--- src/app/deals/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function DealsLoading() {
-  return (
-    <div className="bg-secondary min-h-screen">
-      {/* Mirror the empty page shell background to prevent blank flashes */}
-    </div>
-  );
-}
